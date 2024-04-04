1dabd5d585dc94f6ea30bc7ec987b232c35cad2b
OunissiDhiaEddine <dhia.ounissi@outlook.com>
2024-04-04 07:15:16 +0100

    hid the api

--- commands/feedBack.js
+++ commands/feedBack.js
@@ -3,14 +3,16 @@
 const { SlashCommandBuilder, MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, addDoc } = require('firebase/firestore');
+const { ActionRowBuilder, Events, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+require('dotenv').config();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB0vySYNU8J3ieBqL927WgzzHAO1bqahxI",
-    authDomain: "gdsc-bot-91699.firebaseapp.com",
-    projectId: "gdsc-bot-91699",
-    storageBucket: "gdsc-bot-91699.appspot.com",
-    messagingSenderId: "929785714136",
-    appId: "1:929785714136:web:b1f697f3dc481b79c98ead"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -25,6 +27,8 @@ module.exports = {
         option.setName('text')
           .setDescription('Enter your feedback')
           .setRequired(true)),
+
+    
     async run({ interaction, client, handler }) {
       const feedbackText = interaction.options.getString('text');
       const username = interaction.user.username;
