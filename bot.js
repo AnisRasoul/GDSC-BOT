4c79c33673819eb6f262d67aeb14de4cc8134b31
nouufa <tsnoufa@gmail.com>
2024-04-05 00:01:27 +0100

    first try

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ const client = new Client({ // client = our bot
         IntentsBitField.Flags.GuildMembers, // serverMembers
         IntentsBitField.Flags.GuildMessages, // serverMessages
         IntentsBitField.Flags.MessageContent, //MessageContent
+        IntentsBitField.Flags.GuildMessageReactions //noufa added it for button
     ],
 });
 
@@ -16,8 +17,29 @@ new CommandHandler({
     client,
     commandsPath: path.join(__dirname, 'commands'), // path to commands folder : create commands here
     eventsPath: path.join(__dirname, 'events'), // path to events folder : create events here
-    validationsPath: path.join(__dirname, 'validations'), // path to validations folder : create validations here 
+    /*validationsPath: path.join(__dirname, 'validations'), // path to validations folder : create validations here */
     testServer: process.env.GUILD_ID
 });
+
+
+//noufa added for button 
+/*client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+  });
+  
+  client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand() && !interaction.isButton()) return;
+  
+    try {
+      if (interaction.isCommand()) {
+        await handler.handleCommand(interaction); // Handle slash commands
+      } else if (interaction.isButton()) {
+        await handler.handleButtonInteraction(interaction); // Handle button interactions
+      }
+    } catch (error) {
+      console.error('Error handling interaction:', error);
+    }
+  });
+  //*/
 client.login(process.env.TOKEN)
 
