4b921846e8f11b993287f1350bc5944e16099895
Anis Rasoul <133435049+AnisRasoul@users.noreply.github.com>
2024-04-07 00:21:03 +0200

    Update members.js

--- commands/members.js
+++ commands/members.js
@@ -12,10 +12,11 @@ module.exports = {
                 
                 {name:'Amina Benamor', year:'M2', program:'AI'}],
             HR: [
+                {name:'Amani Rahmani', year:'M1 ', program:'AI'},
                 { name: 'Anis Rasoul', year:'3rd year', program: 'Computer Science' }
             ],
             PR: [
-                { name: 'Roa Salmi', year: '3rd year', program: 'Applied Geology' },
+                { name: 'Khenisssa younes', year: 'M1', program: 'Applied Geology' },
                 { name: 'Skander Benaggoune', year: '5th year', program: 'Industrial Networks & Artificial Intelligence ing' },
                 { name: 'Malak Belabed', year: 'M1', program: 'Foreign languages' }
             ],
@@ -23,22 +24,26 @@ module.exports = {
                 {name:'Dhia Eddine Anis Benflis',year:'M2',program:'ISI'},
                 {name:'Dhia Eddine Ounissi',year:'3rd year',program:'Computer Science '},
                 {name:'Anis Rasoul', year:'3rd year', program: 'Computer Science ' },
-                {name:'Khomri haithem',year:'M1',program:'Ai'}
-
+                {name:'Khomri haithem',year:'M1',program:'Ai'},
+                { name: 'Abdelmalek Medseghir', year: '2nd year', program: 'Computer science' },
 
             ],
             Design: [
+
+                { name: 'Amdjed Nedjahi', year: 'M1', program: 'Accounting and taxation ' },
                 { name: 'Mohamed Boudiaf', year: '3rd year', program: 'Computer Science ' },
                 { name: 'Boughris Menane', year: '2nd Year', program: 'Computer Science ' },
-                { name: 'Abderraouf Benbellat', year: '2nd year', program: 'Computer science , ' },
-                { name: 'Hamza Rouabah', year: '2nd Year', program: 'Computer Science ' }
+                { name: 'Abderraouf Benbellat', year: '2nd year', program: 'Computer science' },
+                { name: 'Hamza Rouabah', year: '2nd Year', program: 'Computer Science' },
+                {name:'Bouragbi salah eddine ', year:'2nd Year', program:'Computer Science'}
             ],
             DM: [
                 { name: 'Amrane Mohamed', year: '2nd year', program: 'Computer Science' },
-                { name: 'Abdelmalek Medseghir', year: '2nd year', program: 'Computer science' },
-                { name: 'Bouthaina Guezouli', year: '2nd Year', program: 'Computer science ' }
+                { name: 'Bouthaina Guezouli', year: '2nd Year', program: 'Computer science ' },
+                { name: 'Amira Benhassir', year: '2nd Year', program: 'Computer science ' }
             ],
             SM: [
+                { name: 'Selma makhloufi', year:'2nd Year', program:'Computer Science'},
                 { name: 'Asma Khettache', year: '2nd year', program: 'CP ST' },
                 { name: 'Zegar Mariaa', year: '2nd year', program: 'Computer science' },
                 { name: 'Saadi Mohamed Younes', year: '2nd year', program: 'Computer science' },
@@ -47,11 +52,11 @@ module.exports = {
             ],
             QC: [
                 { name: 'Aya Lounis', program: 'Artificial Intelligence', year: 'M1' },
-                { name: 'Hadjer Houadef', year: '3rd year', program: 'Foreign languages' },
+                { name: 'Saber Kaddouri', year: '1st year', program: 'Computer Science' },
                 { name: 'Aya Bounafa', year: '2nd year', program: 'Computer Science' }
             ],
             Content: [
-                { name: 'Benhadda Chemselacil', year: 'M1', program: 'Agri-food safety and quality assurance' },
+                { name: 'Chaima Daas', year: 'M1', program: 'AI' },
                 { name: 'Douaa Bouhali', year: '2nd Year', program: 'Computer Science ' },
                 { name: 'Douaa Cherara', year: '2nd Year', program: 'Computer Science ' }
             ],
@@ -87,11 +92,11 @@ module.exports = {
 
             let membersInfo = '';
             selectedMembers.forEach(member => {
-                membersInfo += ` ${member.name} : ${member.year || ''} Student in ${member.program || ''} \n`;
+                membersInfo += `⦿ ${member.name} : ${member.year || ''} Student in ${member.program || ''} \n`;
             });
 
             await i.reply({
-                content: ` Members of ${selection} Team 👨🏻‍💻 :\n  **TeamLeader** : ${membersInfo || 'No members found.'}`,
+                content: ` Members of ${selection} Team 👨🏻‍💻 :\n   **TeamLeader** : ${membersInfo || 'No members found.'}`,
                 ephemeral: true
             });
             
