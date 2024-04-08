15aad2fa3b692d103411e734c4c879abed7825f6
bouthaina <bouthainaguezouli@gmail.com>
2024-04-08 01:28:56 +0100

    messg

--- commands/highlights.js
+++ commands/highlights.js
@@ -7,8 +7,17 @@ module.exports = {
     deleted: false, 
     data: new SlashCommandBuilder() // the slash commands's data here
             .setName('highlights')
-            .setDescription('  bouthaina fill this'),
+            .setDescription('  bouthaina fill this')
+            .addSubcommand((subcommand)=>
+            subcommand
+            .setName('user')
+        .setDescription('description')),
+
+
     run: ({interaction, client, handler}) => { // write the logic here
-        
-    }
-}
+        const subcommand=interaction.options.getSubcommand();
+        if(subcommand==='user'){
+            interaction.reply("reply...");
+        }
+    },
+};
