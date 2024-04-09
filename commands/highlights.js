ec944987d4bc4e946f7d437aa3da7bf012c2ff8f
bouthaina <bouthainaguezouli@gmail.com>
2024-04-09 02:17:33 +0100

    edited

--- commands/highlights.js
+++ commands/highlights.js
@@ -21,6 +21,10 @@ module.exports = {
             subcommand
                 .setName('_ramadan_byte_quiz')
                 .setDescription('View highlights of ramadan.byte quiz ')
+        ) .addSubcommand(subcommand =>
+            subcommand
+                .setName('_mena_tech_skills_24')
+                .setDescription('View highlights of mena tech skills 24')
         ),
     run: async ({ interaction, client, handler }) => {
         const subcommand = interaction.options.getSubcommand();
@@ -30,7 +34,7 @@ module.exports = {
         switch (subcommand) {
             case '_solution_challenge_24':
                 embed = {
-                    title: 'Solution Challenge 24',
+                    title: 'Solution Challenge 24 💡',
                     description: 'Highlights of Solution Challenge 24...',
                     color:0xFEE75C,
                     author: {
@@ -51,13 +55,13 @@ module.exports = {
                            },
                    fields: [
                             { 
-                             name: 'about', 
-                            value: 'solution challenge -event made specialy to try to solve for one of the United Nations’ 17 Sustainable Development Goals using Google technology, Created by the United Nations in 2015 to be achieved by 2030.', 
-                            inline: true,
+                             name: 'About 📃', 
+                            value: ' solution challenge -event made specialy to try to solve for one of the United Nations’ 17 Sustainable Development Goals using Google technology, Created by the United Nations in 2015 to be achieved by 2030.', 
+                           /* inline: true,*/
                             },{
                              name: 'Winner',
                              value: 'unknw',
-                             inline: true,
+                            
                             },
                            ],
                 };
@@ -66,7 +70,7 @@ module.exports = {
       case   '_devfest_23':
 
                 embed = {
-                    title: 'Devfest 23',
+                    title: 'Devfest 23 ',
                     description: 'Highlights of Devfest 23...',
                     color:0xFFFFFF,
                     author: {
@@ -78,7 +82,7 @@ module.exports = {
                     url:'https://gdg.community.dev/events/details/google-gdg-batna-presents-devfest-batna-tech-sessions/',
 
                     image: {
-                        url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTG5d9QMvZc-TuyzeoEEgkFesBtV7M6DyJb71MazUEw8XhGrKoQ',
+                        url: 'https://media.licdn.com/dms/image/D4D22AQHzK4w_e6ledQ/feedshare-shrink_2048_1536/0/1702755801084?e=2147483647&v=beta&t=uuslh6j-ioBLrRsAYwrPeYdRNgWjS4RWwOYtNTQs3Lo',
                     },
                     /*thumbnail: {
                         url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCXVmPInavq6mNdRB2cFBJY3eVP-HsrN7rABWT5f1nbA&s',
@@ -89,14 +93,14 @@ module.exports = {
 	             	icon_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCXVmPInavq6mNdRB2cFBJY3eVP-HsrN7rABWT5f1nbA&s',
                      },
                     fields: [
-                        {   name: 'About',
-                            value:'value', 
-                            inline: true, 
+                        {   name: 'About 📃',
+                            value:'DevFest by Google is a global series of developer events organized by Google Developer Groups (GDGs). It brings together developers to learn about Google technologies, attend workshops, and network with industry experts.', 
+                             
                         },
                         {
                             name: 'Winner',
                             value: 'GDG alger',
-                            inline: true,
+                           
                         },
                     ]
                };
@@ -105,20 +109,20 @@ module.exports = {
      case   '_ramadan_byte_quiz':
 
                     embed = {
-                        title: 'Ramadan.Byte Quiz',
+                        title: 'Ramadan.Byte Quiz🌙',
                         description: 'Highlights of Ramadan.byte quiz...',
                         color:0x206694,
                         author: {
                                 name: 'Ramadan.Byte Quiz 24',
                                 icon_url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa3vbCKBBEiJJbN7nQbt5rQjMdcGv7QOs5WUmGMOofYw&s',
-                                url:'',
+                                url:'https://www.linkedin.com/posts/gdscbatna2_byteramadanquiz-activity-7175932188771684354-caeb',
                                 }, 
     
                         image:  {
                                 url:'https://images-ext-1.discordapp.net/external/3GHgTyGT0ejodnFOIJahSb6GrQzP0TAyob4vFg32kU8/%3F_nc_cat%3D103%26ccb%3D1-7%26_nc_sid%3D5f2048%26_nc_ohc%3Dmimfos6rU3gAb7GiGsH%26_nc_ht%3Dscontent.fblj1-2.fna%26oh%3D00_AfAF38jPfPPrLUbkGGf90Gse5mwZmXHfBslopzMcjJaYbw%26oe%3D6618F671/https/scontent.fblj1-2.fna.fbcdn.net/v/t39.30808-6/432725090_939699931490053_1374859514694416631_n.jpg?format=webp&width=663&height=663',
                                 },  
     
-                        url: '',
+                        url: 'https://www.linkedin.com/posts/gdscbatna2_byteramadanquiz-activity-7175932188771684354-caeb',
                         timestamp: new Date().toISOString(),
                         footer: {
                           text: 'date',
@@ -126,19 +130,57 @@ module.exports = {
                                },
                        fields: [
                                 { 
-                                 name: 'about', 
-                                value: 'value', 
-                                inline: true,
+                                 name: 'About 📃', 
+                                value: 'ByteRamadan Quiz Nights for quick computer science challenges every Saturday, Monday, and Wednesday!  Let’s embark on this knowledge quest together, sparking curiosity and learning along the way! Get ready to challenge your mind and expand your horizons. Don’t miss out!', 
+                               
                                 },
          
-                                {
-                                 name: 'Winner',
+                               /* {
+                                 name: 'field',
                                  value: 'unknw',
-                                 inline: true,
-                                },
+                               
+                                },*/
                                ],
                     };
                     break;
+
+  /*  case   '_mena_tech_skills_24':
+
+                    embed = {
+                        title: 'Mena Tech Skills 24 ',
+                        description: 'Highlights of mena tech skills 24 ...',
+                        color:0x206694,
+                        author: {
+                                name: '',
+                                icon_url:'',
+                                url:'',
+                                }, 
+    
+                        image:  {
+                                url:'',
+                                },  
+    
+                        url: '',
+                        timestamp: new Date().toISOString(),
+                        footer: {
+                          text: 'date',
+                         icon_url: '',
+                               },
+                       fields: [
+                                { 
+                                 name: 'About 📃', 
+                                value: '', 
+                               
+                                },
+         
+                                {
+                                 name: 'field',
+                                 value: 'unknw',
+                               
+                                },
+                               ],
+                    };
+                    break;*/
             default:
                 return interaction.reply({ content: 'Unknown subcommand.', ephemeral: true });
         }
@@ -146,123 +188,4 @@ module.exports = {
         await interaction.reply({ embeds: [embed], ephemeral: true });
     }
 };
-
-/*const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-
-module.exports = {
-    deleted: false,
-    data: new SlashCommandBuilder()
-        .setName('highlightsss')
-        .setDescription('description')  
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('string')
-                .setDescription('Most mportant events in 23/24 season')
-                .addStringOption(option =>
-                    option
-                        .setName('solution_challenge_24')
-                        .setDescription('Description of solution_challenge_24')
-                        .setRequired(true)
-                )
-                .addStringOption(option =>
-                    option
-                        .setName('devfest_23')
-                        .setDescription('Description of devfest_23')
-                        .setRequired(true)),
-                        run: ({ interaction, client, handler }) => {
-                            const embed = {
-                                title: 'Highlights of 23/24 season',
-                                description: 'Here are the most important events:',
-                                fields: [
-                                    { name: 'Solution Challenge 24', value: interaction.options.getString('solution_challenge_24') },
-                                    { name: 'Devfest 23', value: interaction.options.getString('devfest_23') },
-                                    /*{ name: 'Open Day 23', value: interaction.options.getString('open_day_23') },
-                                    { name: 'Ramadan Byte Quiz', value: interaction.options.getString('ramadan_byte_quiz') },
-                                    { name: 'MENA Tech Skills 24', value: interaction.options.getString('mena_tech_skills_24') }
-                                ]
-                            }; 
-                    
-                            interaction.reply({ embeds: [embed], ephemeral: true });
-                        }, ), };*
-        
-   
-              /* .addStringOption(option =>
-                    option
-                        .setName('open_day_23')
-                        .setDescription('Description of open day 23')
-                        .setRequired(true)
-                )
-                .addStringOption(option =>
-                    option
-                        .setName('ramadan_byte_quiz')
-                        .setDescription('Description of ramadan_byte_quiz')
-                        .setRequired(true)
-                )
-                .addStringOption(option =>
-                    option
-                        .setName('mena_tech_skills_24')
-                        .setDescription('Description of mena_tech_skills_24')
-                        .setRequired(true))  */
- 
-
-
-  
-/*run:async ({ interaction, client, handler })=> { 
-    const embed ={ 
-       'one': new EmbedBuilder()
-                .setTitle("solution_challenge_24")
-                .setDescription('description')
-                .setColor('Red'),
-       'two': new EmbedBuilder()
-                .setTitle("devfest_23")
-                .setDescription('description')
-                .setColor('Red'),
-                };
-    await interaction.reply({content: 'Please select an events'
-                           , embed: [embed],ephemeral: true});
-
-
-
-
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = {
-    deleted: false,
-    data: new SlashCommandBuilder()
-        .setName('highlightsss')
-        .setDescription('description')  
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('string')
-                .setDescription('Most important events in 23/24 season')
-                .addStringOption(option =>
-                    option
-                        .setName('solution_challenge_24')
-                        .setDescription('Description of solution_challenge_24')
-                        .setRequired(true)
-                )
-                .addStringOption(option =>
-                    option
-                        .setName('devfest_23')
-                        .setDescription('Description of devfest_23')
-                        .setRequired(true)
-                )
-        ),
-    run: ({ interaction, client, handler }) => {
-        const embed = {
-            title: 'Highlights of 23/24 season',
-            description: 'Here are the most important events:',
-            fields: [
-                { name: 'Solution Challenge 24', value: interaction.options.getString('solution_challenge_24') },
-                { name: 'Devfest 23', value: interaction.options.getString('devfest_23') }
-            ]
-        };
-
-        interaction.reply({ embeds: [embed], ephemeral: true });
-    }
-};   
-
-
-*/
-
 
--- commands/history.js
+++ commands/history.js
@@ -8,23 +8,23 @@ module.exports = {
             .setDescription('tap to show the gdsc history!'),
     run: ({interaction, client, handler}) => { // write the logic here
         interaction.reply({
-            content: `GDSC-history :   
-            Hey there 👋  ! here I'm gonna show you how the GDSC <:gdsc:947931741207789618>  was started.
+            content: `GDSC-history =>   
+            Hey there 👋  ! here I'm gonna show you how the GDSC <:gdsc:947931741207789618>  was started...
 
-The history of GDSC can be traced back to its predecessor, the Google Developer Groups (GDG) program. 
+○ The history of GDSC can be traced back to its predecessor, the Google Developer Groups (GDG) program. 
 
-GDG began as a small initiative in 2008, aiming to bring developers together 
+○ GDG began as a small initiative in 2008, aiming to bring developers together 
 These groups were typically organized by volunteers passionate about technology and Google's platforms.
 
-Over time, GDGs grew in popularity and became a global network of developer communities.
+○ Over time, GDGs grew in popularity and became a global network of developer communities.
 
-In 2016, Google launched the Developer Student Clubs (DSC) program as an extension of GDGs, specifically targeting university students. 
+○ In 2016, Google launched the Developer Student Clubs (DSC) program as an extension of GDGs, specifically targeting university students. 
 
-DSC aimed to provide students with opportunities to learn about Google technologies, collaborate on projects, and network with other students and industry professionals.
+○ DSC aimed to provide students with opportunities to learn about Google technologies, collaborate on projects, and network with other students and industry professionals.
 
-DSC chapters were established in universities worldwide including the chapter university of Batna 2, with student leaders organizing events, workshops, and hackathons.
+○ DSC chapters were established in universities worldwide including the chapter university of Batna 2, with student leaders organizing events, workshops, and hackathons.
 
-In 2019, Google rebranded the DSC program as Google Developer Student Clubs (GDSC) to align it more closely with the broader Google Developer ecosystem.`,
+○ In 2019, Google rebranded the DSC program as Google Developer Student Clubs (GDSC) to align it more closely with the broader Google Developer ecosystem.`,
             ephemeral: true 
         })}};
 
@@ -33,47 +33,3 @@ In 2019, Google rebranded the DSC program as Google Developer Student Clubs (GDS
 
 
 
-
-       /* const { run } = require('./departments');
-
-        module.exports = {
-            data: new SlashCommandBuilder()
-            .setName('ban')
-		.setDescription('Select a member and ban them.')
-		.addUserOption(option =>
-			option
-				.setName('target')
-				.setDescription('The member to ban')
-				.setRequired(true))
-		.addStringOption(option =>
-			option
-				.setName('reason')
-				.setDescription('The reason for banning'))
-		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-		.setDMPermission(false),
-                    async execute(interaction) {
-                        
-                            const target = interaction.options.getUser('target');
-                            const reason = interaction.options.getString('reason') ?? 'No reason provided';
-                     },
-                          run: ({interaction, client, handler}) => {
-                          
-                                 interaction.reply(`Banning ${target.username} for reason: ${reason}`);
-                                 interaction.guild.members.ban(target);
-                                   }
-                        
-                       
-                    };
-                    
-                
-       
-                 /*   async execute(interaction) {
-                        const category = interaction.options.getString('category');
-                        // category must be one of 'gif_funny', 'gif_meme', or 'gif_movie'
-                    }, run: ({interaction, client, handler}) => { // write the logic here
-                    interaction.reply()*/
-             
-            
-        
-    
-                
