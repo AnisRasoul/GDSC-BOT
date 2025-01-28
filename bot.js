0af647a76d0cbfc7013127755dfb15a1671d16a4
AnisRasou <anis.rasoul@etu.univ-batna2.dz>
2025-01-28 14:37:25 +0100

    added express to keep the bot alive

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const {Client,IntentsBitField} = require("discord.js")
 const {CommandHandler} = require('djs-commander')
 const path = require('path')
+const express = require('express')
 
 const client = new Client({
     intents: [
@@ -18,5 +19,18 @@ new CommandHandler({
     commandsPath: path.join(__dirname, 'commands'),
     eventsPath: path.join(__dirname, 'events'),
 });
+
 client.login(process.env.TOKEN)
+
+// Express server to handle API calls to keep the bot alive
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bot is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
